1865a9e1497d5b5ebac64e8e39232ee77f54fc9a
pradeep saini <87126781+pradeepsaini384@users.noreply.github.com>
2022-01-05 22:03:20 +0530

    Update urls.py

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('shop/', include('shop.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^/shop/static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
